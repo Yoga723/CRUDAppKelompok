0b72d3ddaded76d9f4cec2d8542af4105544d585
Yoga723 <yogapangestu16@gmail.com>
2023-12-18 23:42:38 +0700

    Add delete Method and working on Edit

--- app/src/main/java/com/example/crudapp2/MainActivity.kt
+++ app/src/main/java/com/example/crudapp2/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.crudapp2
 
 import android.annotation.SuppressLint
+import android.app.Dialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.ListView
 import android.widget.Toast
@@ -17,8 +20,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
-import com.google.firebase.database.getValue
-import java.util.Objects
+import android.os.Parcelable
 
 class MainActivity : AppCompatActivity() {
 
@@ -39,12 +41,16 @@ class MainActivity : AppCompatActivity() {
         val adapterMahasiswa = AdapterMahasiswa(this, R.layout.list_item_mahasiswa, mahasiswaList)
         listView.adapter = adapterMahasiswa // Set adapter to ListView
 
+        val dialogEdDe = Dialog(this)
+        dialogEdDe.setContentView(R.layout.dialog_edit_delete)
+
 //            Get Data from FB and populate listView
             getDBRef.addValueEventListener(object : ValueEventListener{
 //            OnDatachange these code repeat.
             override fun onDataChange(snapshot: DataSnapshot) {
                 mahasiswaList.clear()
                     println("Ini adalah Datasnapshot :"+ snapshot.toString())
+
                 for(snapshotMahasiswa in snapshot.children){
                     val key = snapshotMahasiswa.key ?: ""
                     val mahasiswa = snapshotMahasiswa.getValue(Mahasiswa::class.java)?.copy(key = key)
@@ -54,15 +60,56 @@ class MainActivity : AppCompatActivity() {
                     println("Ini adalah mahasiswa :"+ mahasiswa.toString())
 
                     if (mahasiswa != null) {
-                        mahasiswaList.add(mahasiswa) // Add each Mahasiswa to the list
+                        mahasiswaList.add(mahasiswa) // Add tiap Mahasiswa to the list
                     }
-                    adapterMahasiswa.notifyDataSetChanged() // Notify the adapter that the data has changed
+                    adapterMahasiswa.notifyDataSetChanged() // Notify adapter data has changed
                 }
             }
             override fun onCancelled(error: DatabaseError) {
                 println("Failed to read Value." + error.toException())
             }
         })
+
+//        Show Dialog Delete/Edit Options
+        listView.setOnItemClickListener { parent, view, dataMhs, id ->
+            dialogEdDe.show()
+            val btnDelete = dialogEdDe.findViewById<Button>(R.id.btnDelete)
+            val btnEdit = dialogEdDe.findViewById<Button>(R.id.btnEdit)
+
+            val selectedMahasiswa = mahasiswaList[dataMhs]
+            val getDataId = getDBRef.child(selectedMahasiswa.key) // Get Mahasiswa dengan key sama
+
+//            Edit Actions
+            btnEdit.setOnClickListener {
+                val intent = Intent(this@MainActivity, UpdateActivity::class.java)
+
+                intent.putExtra("key", selectedMahasiswa.key)
+                intent.putExtra("nama", selectedMahasiswa.nama)
+                intent.putExtra("nim", selectedMahasiswa.nim.toString())
+                intent.putExtra("tempatLahir", selectedMahasiswa.tempatLahir)
+                startActivity(intent)
+//                getDataId.
+//                    .addOnSuccessListener {
+//                        dialogEdDe.hide()
+//                        Toast.makeText(this, "Data Mahasiswa Dihapus", Toast.LENGTH_LONG).show()
+//                    }
+//                    .addOnFailureListener{
+//                        Toast.makeText(this, "Data Mahasiswa Gagal di Hapus, Coba Lagi !", Toast.LENGTH_LONG).show()
+//                    }
+            }
+
+//            Delete Actions
+            btnDelete.setOnClickListener {
+                getDataId.removeValue()
+                    .addOnSuccessListener {
+                        dialogEdDe.hide()
+                        Toast.makeText(this, "Data Mahasiswa Dihapus", Toast.LENGTH_LONG).show()
+                    }
+                    .addOnFailureListener{
+                        Toast.makeText(this, "Data Mahasiswa Gagal di Hapus, Coba Lagi !", Toast.LENGTH_LONG).show()
+                    }
+            }
+        }
 
         AddButton.setOnClickListener {
             replaceFragment(AddFragment())
